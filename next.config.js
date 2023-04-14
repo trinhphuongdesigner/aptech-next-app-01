443a09f5c9ae8dbe8c8b22939f067a4129647d3f
@phuongtd.wiicamp <phuongtd@wiicamp.com>
2023-04-14 21:17:51 +0700

    Update query

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: [
+      'pinterest.com',
+      'images.unsplash.com',
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
